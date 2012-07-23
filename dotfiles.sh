059f43aacbaf8a846e84ccf014ba5a565b1918d1
wikitopian <matt@swarmstrategies.com>
2012-07-22 20:58:25 -0400

    Automate desktop stuff

---
+++ dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ln -s ~/Dropbox/admin/dotfiles/.bashrc ~/.bashrc
+ln -s ~/Dropbox/admin/dotfiles/.devilspie ~/.devilspie
+ln -s ~/Dropbox/admin/dotfiles/chrome.desktop ~/.config/autostart
+ln -s ~/Dropbox/admin/dotfiles/VT1.desktop ~/.config/autostart
+ln -s ~/Dropbox/admin/dotfiles/VT2.desktop ~/.config/autostart
+ln -s ~/Dropbox/admin/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Dropbox/admin/dotfiles/.hgrc ~/.hgrc
+ln -s ~/Dropbox/admin/dotfiles/.muttrc ~/.muttrc.
+ln -s ~/Dropbox/admin/dotfiles/.screenrc ~/.screenrc
+ln -s ~/Dropbox/admin/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Dropbox/admin/dotfiles/.topen ~/.topen
+chmod +x ~/.topen

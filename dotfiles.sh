59fca96f640e876bb567cd3774f6ecbc97d8549d
wikitopian <matt@swarmstrategies.com>
2013-02-28 23:10:22 -0500

    General cleanup

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,13 +5,8 @@ ln -s .vimrc ~/.vimrc
 
 ln -s .Xmodmap ~/.Xmodmap
 
-ln -s .ratpoisonrc ~/.ratpoisonrc
-
 ln -s .tmux.conf ~/.tmux.conf
 
 ln -s ~/Dropbox/admin/ssh-config ~/.ssh/config
 
 ln -s ~/Dropbox/admin/backuprc.secret ~/.backuprc
-
-ln -s .ctags ~/.ctags
-ctags -R -o ~/.tags WordPress/ wordpress-jquerymobile
